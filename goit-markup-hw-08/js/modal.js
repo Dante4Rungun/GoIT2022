3c1173b8c4e188d1ad1dbfbee1005c1bb85c64dc
Dante4Rungun <denischuprin9619@gmail.com>
2022-03-07 13:48:43 +0200

    Update modal.js

--- goit-markup-hw-08/js/modal.js
+++ goit-markup-hw-08/js/modal.js
@@ -3,23 +3,12 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-
-    openModalBtnHeader: document.querySelector("[header-modal-open]"),
-    closeModalBtnHeader: document.querySelector("[header-modal-close]"),
-    modalHeader: document.querySelector("[header-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  refs.openModalBtnHeader.addEventListener("click", toggleModalHeader);
-  refs.closeModalBtnHeader.addEventListener("click", toggleModalHeader);
-
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
-  }
-
-  function toggleModalHeader() {
-    refs.modalHeader.classList.toggle("is-hidden");
   }
 })();
