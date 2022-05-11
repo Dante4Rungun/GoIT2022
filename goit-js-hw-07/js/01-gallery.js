8d0e68bc49577dfbf0fcdabb018ba382e065db92
Dante4Rungun <denischuprin9619@gmail.com>
2022-05-11 19:30:58 +0300

    Add files via upload

---
+++ goit-js-hw-07/js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from './gallery-items.js';
+//console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector(".gallery")
+//const link = document.querySelector(".gallery__link")
+
+gallery.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src = ${event.target.getAttribute("data-source")}>
+    `)
+    instance.show()
+})
+
+function createGallery() {
+    const items = []
+
+    for(const element of galleryItems){
+        const item = document.createElement("div")
+        item.classList.add("gallery__item")
+
+        const link = document.createElement("a")
+        link.classList.add("gallery__link")
+        link.href = element.original
+
+        const img = document.createElement("img")
+        img.src = element.preview
+        img.alt = element.description
+        img.classList.add("gallery__image")
+        img.setAttribute("data-source",element.original)
+
+        link.append(img)
+        item.append(link)
+        items.push(item)
+    }
+    console.log(items)
+}
+
+createGallery()
---
+++ goit-js-hw-07/js/02-lightbox.js
@@ -0,0 +1,38 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector(".gallery")
+
+function createGallery() {
+    const items = []
+
+    for(const element of galleryItems){
+        const item = document.createElement("div")
+        item.classList.add("gallery__item")
+
+        const link = document.createElement("a")
+        link.classList.add("gallery__link")
+        link.href = element.original
+
+        const img = document.createElement("img")
+        img.src = element.preview
+        img.alt = element.description
+        img.classList.add("gallery__image")
+
+        link.append(img)
+        item.append(link)
+        items.push(item)
+    }
+    gallery.append(...items)
+}
+
+
+
+createGallery()
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault()
+    let gallery = new SimpleLightbox('.gallery a');
+    gallery.on('show.simplelightbox', function () {
+        gallery.navText = event.target.getAttribute("alt")
+    });
+})
