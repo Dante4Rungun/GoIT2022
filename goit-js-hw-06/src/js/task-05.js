2058040a40d3b75466c65ab86d9860a3bad47d13
Dante4Rungun <denischuprin9619@gmail.com>
2022-05-13 17:37:33 +0300

    Delete goit-js-hw-06/src/js directory

--- goit-js-hw-06/src/js/task-05.js
+++
@@ -1,9 +0,0 @@
-const inp = document.getElementById("name-input")
-const out= document.getElementById("name-output")
-
-inp.addEventListener("input", (event)=>{
-    out.textContent = event.target.value
-    if(event.target.value === ''){
-        out.textContent = "Anonymus"
-    }
-})
--- goit-js-hw-06/src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const range = document.getElementById("font-size-control")
-const text = document.getElementById("text")
-
-range.addEventListener("input", (event) => {
-    text.style.fontSize = `${event.target.value}px`
-})
