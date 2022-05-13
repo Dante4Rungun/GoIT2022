eb6221d96814823ccacaac56efde5736983e01e9
Dante4Rungun <denischuprin9619@gmail.com>
2022-05-13 17:48:56 +0300

    Update task-08.js

--- goit-js-hw-06/src/js/task-08.js
+++ goit-js-hw-06/src/js/task-08.js
@@ -1,16 +1,19 @@
 const form = document.querySelector(".login-form")
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+    
 
     const {elements:{email,password}} = event.currentTarget
 
     if (email.value === "" || password.value === ""){
+        event.preventDefault()
         alert('Please input all data')
     }
     else{
-        console.log(`Login: ${email.value} \nPassword: ${password.value}`)
-        console.log()
+        //console.log(`Login: ${email.value} \nPassword: ${password.value}`)
+        console.log({email: email.value, password: password.value})
+        event.preventDefault()
+        event.currentTarget.reset()
     }
 
     return false
